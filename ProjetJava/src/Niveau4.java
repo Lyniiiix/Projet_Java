d5e063dd3990bb555d01caf26e6765787e570dc5
Ruru-TheBigY <rubenlegourrierec@gmail.com>
2023-03-30 13:30:40 +0200

    plateforme mobile

--- ProjetJava/src/Niveau4.java
+++ ProjetJava/src/Niveau4.java
@@ -7,15 +7,26 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class Niveau4 extends BasicGameState {
 	private Personnage joueur ;
+	private Plateforme p1, p2, p3, p4, p5;
 
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		joueur = new Personnage();
+		p1= new Plateforme(100, 650, 1000); // la 3 eme cordonnee c le temps pour le changement de direction
+		p2= new Plateforme(500, 600, 1000);
+		p3= new Plateforme(350, 500, 1000);
+		p4= new Plateforme(200, 400, 1000);
+		p5= new Plateforme(600, 250, 1000);
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		joueur.dessiner(g);
+		p1.draw(g);
+		p2.draw(g);
+		p3.draw(g);
+		p4.draw(g);
+		p5.draw(g);
 	}
 
 	@Override
@@ -23,6 +34,12 @@ public class Niveau4 extends BasicGameState {
 		joueur.sauter(delta);
 		joueur.gravite(delta);
 		
+
+		p1.deplacement(delta);
+		p2.deplacement(delta);
+		p3.deplacement(delta);
+		p4.deplacement(delta);
+		p5.deplacement(delta);
 		
 		
 		Input mvt = gc.getInput();
--- ProjetJava/src/Plateforme.java
+++ ProjetJava/src/Plateforme.java
@@ -1,4 +1,49 @@
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
 
 public class Plateforme {
+	private float x,y,vx,vy;
+	private int timerChangementDirection, time;
+	private Color c;
+	
+	public Plateforme(float x, float y, int timerChangementDirection) {
+		c = Color.white;
+		vx=5;
+		vy=0;
+		
+		if(x>=20 && x<=1024-20) { // le 20 correspond a la bordure
+			this.x=x;
+		}
+		else
+			this.x=100;
+		if(y>=20 && y<=720-20) { // le 20 correspond a la bordure
+			this.y=y;
+		}
+		else
+			this.y=500;
+		
+		if(timerChangementDirection>=0)
+			this.timerChangementDirection=timerChangementDirection;
+		else
+			timerChangementDirection=1000;
+	}
+	
+	
+	public void draw(Graphics g) {
+		g.setColor(c);
+		g.fillRect(x, y, 100, 20);
+	}
+	
+	public void deplacement(int delta) {
 
+		time+=delta;
+		
+		if(time >= timerChangementDirection) {
+			time=0;
+			vx = -vx;
+		}
+		
+		x+=vx;
+	}
 }
+
