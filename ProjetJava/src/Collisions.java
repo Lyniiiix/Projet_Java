04cbdfe2be0625c20a9bad6ebbc6d952991e4472
Lynix <alicn3287@gmail.com>
2023-05-05 09:56:12 +0200

    Collision

--- ProjetJava/src/Collisions.java
+++ ProjetJava/src/Collisions.java
@@ -1,3 +1,29 @@
+import org.newdawn.slick.tiled.TiledMap;
+
 public class Collisions {
+	
+	public static boolean collision(TiledMap map)
+	{
+		
+		int mapWidth = map.getWidth();
+		int mapHeight = map.getHeight();
+
+		for (int y = 0; y < mapHeight; y++) {
+		    for (int x = 0; x < mapWidth; x++) {
+		        int tileId = map.getTileId(x, y, 1); // layer d'index 1
+		        if (tileId == 0) {
+		            System.out.print("0 ");
+		        } else {
+		            System.out.print("1 ");
+		        }
+		    }
+		    System.out.println();
+		}
+
+
+
+		
+		return false;
+	}
 	
 }
